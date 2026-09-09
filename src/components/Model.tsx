6093299d395f43c41798186350c3fcb144626876
Eddie Johansson <eddie.johanssonn@hotmail.com>
2026-09-09 11:25:55 +0200

    Applied starter file structure

--- src/components/Model.tsx
+++
@@ -1,9 +0,0 @@
-import { useGLTF } from "@react-three/drei";
-
-export default function Model() {
-  const { scene } = useGLTF("/VP_MOCK_Test_1.glb");
-
-  console.log(scene)
-
-  return <primitive object={scene} />;
-}
