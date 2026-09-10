2c8a33465547496e551d1139e13d278f23b21b47
Eddie Johansson <eddie.johanssonn@hotmail.com>
2026-09-10 11:22:38 +0200

    applied small css to Configurator

--- src/components/Configurator/Configurator.tsx
+++ src/components/Configurator/Configurator.tsx
@@ -3,14 +3,17 @@ import ModelViewer from "../ModelViewer/ModelViewer";
 import Footer from "../Footer/Footer";
 import OptionSelection from "../OptionSelection/OptionSelection";
 import FeatureSelection from "../FeatureSelection/FeatureSelection";
+import styles from "./Configurator.module.css"
 
 function Configurator() {
   return (
     <main>
       <Header />
-      <OptionSelection />
-      <ModelViewer />
-      <FeatureSelection />
+      <section className={styles.contentWrapper}>
+        <OptionSelection />
+        <ModelViewer />
+        <FeatureSelection />
+      </section>
       <Footer />
     </main>
   );
