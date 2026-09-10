418642058263d1f2f8a56ce1030b0a695e652270
Eddie Johansson <eddie.johanssonn@hotmail.com>
2026-09-10 12:42:46 +0200

    Added css to ModelViewer and Footer

--- src/components/ModelViewer/ModelViewer.tsx
+++ src/components/ModelViewer/ModelViewer.tsx
@@ -1,6 +1,7 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import { Suspense } from "react";
+import styles from "./ModelViewer.module.css"
 
 function VinylPlayer() {
     const { scene } = useGLTF('/models/VP_MOCK_Test_1.glb')
@@ -10,8 +11,8 @@ function VinylPlayer() {
 
 function ModelViewer() {
   return (
-    <div>
-      <Canvas>
+    <div className={styles.viewer}>
+      <Canvas camera={{ position: [3, 2, 5], fov: 50 }}>
         <ambientLight intensity={1} />
         <directionalLight position={[5, 5, 5]} intensity={2} />
         
