ba6d53058589438c1143e459f623aa950e557086
Eddie Johansson <eddie.johanssonn@hotmail.com>
2026-09-14 16:32:28 +0200

    imported svg for 3D button

--- src/components/View3D/View3D.tsx
+++ src/components/View3D/View3D.tsx
@@ -1,14 +1,18 @@
 import { useConfigurator } from "../../hooks/useConfigurator";
 import View3DModel from "./View3DModel";
 import styles from "./View3D.module.css"
+import viewIn3D from "../../assets/images/view-in-3d.svg"
 
 function View3D() {
   const { is3DOpen, setIs3DOpen } = useConfigurator();
 
   return (
     <>
-      <button onClick={() => setIs3DOpen(true)}>
-        View in 3D
+      <button
+        className={styles.viewButton}
+        onClick={() => setIs3DOpen(true)}
+          >
+        <img src={viewIn3D} alt="View in 3D" />
       </button>
 
       {is3DOpen && (
