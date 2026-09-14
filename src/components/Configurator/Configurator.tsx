427c777327457f317a1e0b1fb7aed467605502fa
Robin Andersson <danrobin.andersson@outlook.com>
2026-09-14 10:24:46 +0200

    removed unused file

--- src/components/Configurator/Configurator.tsx
+++ src/components/Configurator/Configurator.tsx
@@ -4,7 +4,7 @@ import Footer from "../Footer/Footer";
 import OptionSelection from "../OptionSelection/OptionSelection";
 import FeatureSelection from "../FeatureSelection/FeatureSelection";
 import styles from "./Configurator.module.css";
-import { ConfiguratorProvider, useConfigurator, } from "../../hooks/useConfigurator";
+import { ConfiguratorProvider } from "../../hooks/useConfigurator";
 
 function Configurator() {
   return (
