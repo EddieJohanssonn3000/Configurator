15823a43d0e4a33b23d61af40820eba06b76ac18
Eddie Johansson <eddie.johanssonn@hotmail.com>
2026-09-14 14:37:25 +0200

    fixed activeStep depending on where in menu

--- src/components/ViewToggle/ViewToggle.tsx
+++ src/components/ViewToggle/ViewToggle.tsx
@@ -4,6 +4,7 @@ import styles from "./ViewToggle.module.css"
 
 function ViewToggle() {
   const {
+    activeStep,
     sliderValue,
     handleRotationChange,
   } = useConfigurator();
@@ -11,6 +12,18 @@ function ViewToggle() {
   return (
 
     <div className={styles.container}>
+
+       {activeStep === "Design" && (
+      <h1 className={styles.title}>Design - Choose your design</h1>
+    )}
+    
+    {activeStep === "Functions" && (
+      <h1 className={styles.title}>Function - Pick your function</h1>
+    )}
+
+    {activeStep === "Custom" && (
+      <h1 className={styles.title}>Custom - Customize your player</h1>
+    )}
     
     <input className={styles.slider}
       type="range"
