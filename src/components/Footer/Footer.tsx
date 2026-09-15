5ae094485c1e72336faa0071d4cbbc1e3a050e43
Eddie Johansson <eddie.johanssonn@hotmail.com>
2026-09-15 10:08:45 +0200

    Fixed Footer-bottoms

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,48 @@
-import styles from "./Footer.module.css"
+import styles from "./Footer.module.css";
+import { useConfigurator } from "../../hooks/useConfigurator";
 
 function Footer() {
-    return (
-       <footer className={styles.footerBtn}>
-        <button>Previous</button>
-        <button>Next</button>
-       </footer>
-    );
+  const { activeStep, setActiveStep } = useConfigurator();
+
+  const handlePrevious = () => {
+    if (activeStep === "Functions") {
+      setActiveStep("Design");
+    }
+
+    if (activeStep === "Custom") {
+      setActiveStep("Functions");
+    }
+  };
+
+  const handleNext = () => {
+    if (activeStep === "Design") {
+      setActiveStep("Functions");
+    }
+
+    if (activeStep === "Functions") {
+      setActiveStep("Custom");
+    }
+  };
+
+  return (
+    <footer className={styles.footerBtn}>
+      {activeStep !== "Design" && (
+        <button onClick={handlePrevious}>Previous</button>
+      )}
+
+      {activeStep === "Design" && (
+        <button onClick={handleNext}>Next</button>
+      )}
+
+      {activeStep === "Functions" && (
+        <button onClick={handleNext}>Next</button>
+      )}
+
+      {activeStep === "Custom" && (
+        <button>Done</button>
+      )}
+    </footer>
+  );
 }
 
 export default Footer;
