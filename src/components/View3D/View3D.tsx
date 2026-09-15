651c4b167038883bd58a775f57cd8ee72d2017bd
Eddie Johansson <eddie.johanssonn@hotmail.com>
2026-09-15 10:38:54 +0200

    added infoCircle

--- src/components/View3D/View3D.tsx
+++ src/components/View3D/View3D.tsx
@@ -2,18 +2,26 @@ import { useConfigurator } from "../../hooks/useConfigurator";
 import View3DModel from "./View3DModel";
 import styles from "./View3D.module.css"
 import viewIn3D from "../../assets/images/view-in-3d.svg"
+import infoIcon from "../../assets/images/circle-outline.svg"
+
 
 function View3D() {
   const { is3DOpen, setIs3DOpen } = useConfigurator();
 
   return (
     <>
+    <div className={styles.actions}>
+      <button className={styles.infoButton}>
+        <img src={infoIcon} alt="Information" />
+      </button>
+
       <button
         className={styles.viewButton}
         onClick={() => setIs3DOpen(true)}
-          >
+      >
         <img src={viewIn3D} alt="View in 3D" />
       </button>
+    </div>
 
       {is3DOpen && (
        <div className={styles.overlay}>
