f201006e938000204f1a2824e3b21b058fb0ce18
Eddie Johansson <eddie.johanssonn@hotmail.com>
2026-09-15 10:56:08 +0200

    Added img and buttons to lidOpen and lidClosed

--- src/components/View3D/View3D.tsx
+++ src/components/View3D/View3D.tsx
@@ -3,6 +3,8 @@ import View3DModel from "./View3DModel";
 import styles from "./View3D.module.css"
 import viewIn3D from "../../assets/images/view-in-3d.svg"
 import infoIcon from "../../assets/images/circle-outline.svg"
+import lidClosed from "../../assets/images/LidClosed.svg";
+import lidOpen from "../../assets/images/LidOpen.svg";
 
 
 function View3D() {
@@ -11,6 +13,16 @@ function View3D() {
   return (
     <>
     <div className={styles.actions}>
+      <div className={styles.lidButtons}>
+        <button className={styles.lidButton}>
+          <img src={lidClosed} alt="Closed lid" />
+        </button>
+
+        <button className={styles.lidButton}>
+          <img src={lidOpen} alt="Open lid" />
+        </button>
+      </div>
+
       <button className={styles.infoButton}>
         <img src={infoIcon} alt="Information" />
       </button>
