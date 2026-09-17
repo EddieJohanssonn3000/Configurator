8981d820869a4a6a8360d020f2c6136db2485e7a
Robin Andersson <danrobin.andersson@outlook.com>
2026-09-17 13:18:11 +0200

    added stereo feature option

--- src/data/configuratorOptions.ts
+++ src/data/configuratorOptions.ts
@@ -12,7 +12,7 @@ export const FEATURES_BY_STEP = {
   Functions: [
     { key: "bluetooth", label: "Bluetooth", options: ["on", "off"] },
     { key: "light", label: "Light", options: ["on", "off"] },
-    { key: "stereo", label: "Stereo", options: ["on", "off"] },
+    { key: "body", label: "Stereo", options: ["on", "off"] },
   ],
   Custom: [
     { key: "stickers", label: "Stickers", options: ["none", "logo"] },
